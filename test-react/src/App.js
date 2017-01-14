f1de25cb0e3c6825e6047df24f0291eb9ee05c10
Szabo Rihcard <ignocius@gmail.com>
2017-01-14 13:00:15 +0100

    server added css deleted

--- test-react/src/App.js
+++ test-react/src/App.js
@@ -3,18 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Cica from './testComponent.js';
 
+// console.log("123");
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Cica />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <article>
+          <h1>React All the ToDos</h1>
+        </article>
       </div>
     );
   }
