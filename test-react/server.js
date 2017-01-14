7720082458317d98f0b8e69e3662770f1e311052
dunaicapa <dunaicapa@dunaicapa.hu>
2017-01-14 13:43:56 +0100

    socket io

--- test-react/server.js
+++ test-react/server.js
@@ -5,6 +5,8 @@ var app         = express();
 var server      = http.createServer(app);
 var io          = require('socket.io').listen(server);
 
+server.listen(3001);
+
 app.set('views', __dirname + '/public');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine({ beautify: true }));
@@ -14,21 +16,18 @@ app.get('/', function (req, res) {
   //res.render('index',{name:"almanegra2"});
 });
 
-
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  io.emit('chat message', config.chat.welcomeString);
+  io.emit('receive', 'Welcome');
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('sendevent', function(msg){
+    io.emit('receive', msg);
   });
 });
-
-//server.listen(app.get('port'));
 
 app.listen(1337, function () {
   console.log('Example app listening on port 1337!')
